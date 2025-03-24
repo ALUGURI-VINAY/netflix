5bdc7cd3b5376fcad6663b3893fb6a031c603181
ALUGURI-VINAY <165195217+ALUGURI-VINAY@users.noreply.github.com>
2025-03-24 20:58:34 +0530

    Update Next.js configuration for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/netflix',
+  assetPrefix: '/netflix',
+}
+
+module.exports = nextConfig
